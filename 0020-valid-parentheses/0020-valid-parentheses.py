54e0788051f105f294f9038fa24b2b8070446ffd
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-01 22:56:10 +0530

    Time: 12 ms (91.97%), Space: 13.5 MB (61.84%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack=[]
+
+        for i in s:
+            if i in '({[':
+                stack.append(i)
+            else:
+                if not stack or (i ==')' and stack[-1] !='(') or (i =='}' and stack[-1] !='{') or (i ==']' and stack[-1] !='['):
+                    return False
+                stack.pop()
+        if len(stack)==0:
+            return True
+        return False
+        
+
+
