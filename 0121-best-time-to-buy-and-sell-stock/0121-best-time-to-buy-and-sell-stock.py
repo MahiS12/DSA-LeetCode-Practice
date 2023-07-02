bb88444cce8c3a3d2fd3ce5ab2138ea43864ffc4
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-02 16:41:01 +0530

    Time: 919 ms (32.16%), Space: 22.5 MB (62.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,30 +4,30 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-#         i = 0
-#         j =1
-#         profit = 0
+        i = 0
+        j =1
+        profit = 0
         
-#         while j < len(prices):
-#             curr = prices[j] - prices[i]
+        while j < len(prices):
+            curr = prices[j] - prices[i]
             
-#             if prices[j] > prices[i]:
-#                 profit = max(profit,curr)
-#             else:
-#                 i +=1
-#             j+=1
-#         return profit
+            if prices[j] > prices[i]:
+                profit = max(profit,curr)
+            else:
+                i = j
+            j+=1
+        return profit
     
 
-        left = 0 #Buy
-        right = 1 #Sell
-        max_profit = 0
-        while right < len(prices):
-            currentProfit = prices[right] - prices[left] #our current Profit
-            if prices[left] < prices[right]:
-                max_profit =max(currentProfit,max_profit)
-            else:
-                left = right
-            right += 1
-        return max_profit
+#         left = 0 #Buy
+#         right = 1 #Sell
+#         max_profit = 0
+#         while right < len(prices):
+#             currentProfit = prices[right] - prices[left] #our current Profit
+#             if prices[left] < prices[right]:
+#                 max_profit =max(currentProfit,max_profit)
+#             else:
+#                 left = right
+#             right += 1
+#         return max_profit
         
