a00ca6a5c8b85fdecf9389c8fb40ec8597d4e018
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-02 18:13:26 +0530

    Time: 23 ms (31.06%), Space: 13.4 MB (94.56%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -6,22 +6,44 @@ class Solution(object):
         :rtype: bool
         """
         
-        i = j = count =0
-        
-        if len(s) > len(t):
-            return False
-        if len(s)== 0 or len(t) ==0:
+        i,j = 0,0
+        n,m = len(t), len(s)
+        if m == 0:
             return True
         
+        overlap = 0
+        while i < n and j < m:
+            if t[i] == s[j]:
+                i += 1
+                j += 1
+                overlap += 1
+            else:
+                i += 1
+        return overlap == m
         
-        while j < len(t) and i <len(s):
-            if s[i] == t[j]:
-                i+=1
-                count+=1
-            j+=1
+        
+#         i = j = count =0
+        
+#         if len(s) > len(t):
+#             return False
+#         if len(s)== 0 or len(t) ==0:
+#             return True
+        
+        
+#         while j < len(t) and i <len(s):
+#             if s[i] == t[j]:
+#                 i+=1
+#                 count+=1
+#             j+=1
         
        
-        if count >= len(s):
-            return True
-        else:
-            return False
+#         if count >= len(s):
+#             return True
+#         else:
+#             return False
+
+
+
+
+
+        
