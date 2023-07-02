bc5e1b3ab5af51adf2477e3350837aebe63e945d
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-02 22:51:35 +0530

    Time: 14 ms (76.50%), Space: 13.7 MB (29.17%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -5,26 +5,21 @@ class Solution(object):
         :rtype: int
         """
         
-        s = s.rstrip()
-        return len(s.split()[-1])
+        count = 0
         
-#         class Solution:
-#     def lengthOfLastWord(self, s: str) -> int:
-#         cnt = 0;
-#         for i in range(len(s)-1,-1,-1):
-#             if(s[i]== ' '):
-#                 if cnt>0 :
-#                     return cnt
-#                 continue
-#             cnt+=1;
-#         return cnt
-# 2.
-
-# class Solution:
-#     def lengthOfLastWord(self, s: str) -> int:
-#         s = s.rstrip()  #or strip
-#         return len(s.split()[-1])
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == ' ':
+                if count >0:
+                    return count
+                continue
+            count+=1
+        return count
             
+        
+        # s = s.rstrip()
+        # return len(s.split()[-1])
+        
+
             
         
         
