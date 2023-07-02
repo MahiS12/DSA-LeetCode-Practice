4aa13c97a89206c8a39111d6a8921e9fe03c8e0a
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-02 16:14:58 +0530

    Time: 133 ms (86.66%), Space: 14.7 MB (92.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,14 +5,38 @@ class Solution(object):
         :rtype: int
         """
         
-        d ={}
-        
-        for i in nums:
-            if i not in d:
-                d[i] = 1
+
+        count = 1
+        majority = nums[0]
+
+        for num in nums[1:]:
+            if num != majority:
+                count -= 1
+                if count == 0:
+                    majority = num
+                    count = 1
             else:
-                d[i] +=1
+                count += 1
+
+        return majority    
+        
+        
+        
+        
+        
+        
+        
+        
+        # d ={}
+        
+#         for i in nums:
+#             if i not in d:
+#                 d[i] = 1
+#             else:
+#                 d[i] +=1
             
         
-        return max(d, key = lambda x:d[x])
+#         return max(d, key = lambda x:d[x])
+    
+        
         
