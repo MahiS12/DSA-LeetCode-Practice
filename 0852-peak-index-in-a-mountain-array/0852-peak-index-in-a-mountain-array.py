230c62846dbc2607fe65f6a3313b7000ecc85ad4
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-06 02:47:41 +0530

    Time: 565 ms (9.04%), Space: 25.8 MB (65.48%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -9,18 +9,19 @@ class Solution(object):
         low = 0
         high = len(arr)-1
         
-        while low <high:
+        while low <=high:
             
             mid = low + (high-low)//2
+            if arr[mid]>=arr[mid-1] and arr[mid]>=arr[mid+1]:
+                return mid
             
-            
-            if arr[mid] > arr[mid+1]:
-                high = mid  #checking the left half of the array for greater element
+            if arr[mid] > arr[mid+1]: #decreasing part of array, look in left
+                high = mid-1  #checking the left half of the array for greater element
                 
-            elif arr[mid] < arr[mid+1]:
+            elif arr[mid] < arr[mid+1]: #look in right
                 low = mid +1 #checking the right half or array for greater
                 
-        return low
+#         return low
             
             
             
