1008faaa8170a04015c9834759c36ece7741b736
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-06 03:04:18 +0530

    Time: 32 ms (57.51%), Space: 13.3 MB (97.16%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -11,31 +11,21 @@ class Solution(object):
         while low <  high:
             mid= low +(high-low)//2
 
-            if mid ==0:
-                return 0 if nums[0] >= nums[1] else 1
-            if mid == high:
-                return high-1 if nums[high-1] >= nums[high-2] else high-2
+#             if mid ==0:
+#                 return 0 if nums[0] >= nums[1] else 1
+#             if mid == high:
+#                 return high-1 if nums[high-1] >= nums[high-2] else high-2
 
-            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
-                return mid
-            elif nums[mid] > nums[mid-1]:
+#             if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+#                 return mid
+            if nums[mid] < nums[mid+1]:
                 low = mid+1
-            else:
-                high = mid-1
+            elif nums[mid] > nums[mid+1]:
+                high = mid
         return low
     
     
-#         while low <high:
-            
-#             mid = low +(high-low)//2
-            
-#             if nums[mid] > nums[mid+1]:
-#                 high = mid
-#             elif nums[mid] > nums[mid-1]:
-#                 low = mid+1
-                
-#         return low
-                
+
          
     
 
