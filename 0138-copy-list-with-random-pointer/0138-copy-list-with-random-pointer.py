e259ac466392ae46e9deba8e819f4dfeb3b4fb6e
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-08 14:22:22 +0530

    Time: 40 ms (78.65%), Space: 14 MB (57.65%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -13,23 +13,54 @@ class Solution(object):
         :type head: Node
         :rtype: Node
         """
+        if not head:
+            return None
+
+    # Step 1: Create copies of nodes
+        current = head
+        while current:
+            new_node = Node(current.val, current.random)
+            new_node.next = current.next
+            current.next = new_node
+            current = new_node.next
+
+    # Step 2: Set random pointers
+        current = head
+        while current:
+            if current.random is not None:
+                current.next.random = current.random.next
+            current = current.next.next
+
+    # Step 3: Split the lists
+        current = head
+        new_head = head.next
+        new_current = new_head
+        while current:
+            current.next = new_current.next
+            current = current.next
+            if new_current.next:
+                new_current.next = new_current.next.next
+            new_current = new_current.next
+
+        return new_head
         
-        copyMap = {None:None}
         
-        curr = head
+#         copyMap = {None:None}
         
-        while curr:
-            copy = Node(curr.val)
-            copyMap[curr] = copy
-            curr= curr.next
+#         curr = head
+        
+#         while curr:
+#             copy = Node(curr.val)
+#             copyMap[curr] = copy
+#             curr= curr.next
             
         
-        curr = head
+#         curr = head
         
-        while curr:
-            copy = copyMap[curr]
-            copy.next = copyMap[curr.next]
-            copy.random = copyMap[curr.random]
-            curr= curr.next
+#         while curr:
+#             copy = copyMap[curr]
+#             copy.next = copyMap[curr.next]
+#             copy.random = copyMap[curr.random]
+#             curr= curr.next
         
-        return copyMap[head]
+#         return copyMap[head]
