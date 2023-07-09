d694adafcdfe0b97d582145d74659b4a53e53bf2
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-10 01:44:52 +0530

    Time: 1393 ms (70.91%), Space: 20.5 MB (44.48%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -23,9 +23,14 @@ class Solution:
                 else:
                     res.append([nums[i],nums[left],nums[right]])
                     
-                    left +=1
+                    left +=1 #updating the left pointer
+                #checking if its the same value as before to avoid duplicates
                     while nums[left] == nums[left-1] and left < right:
-                        left +=1
+                        left +=1 
+                    while nums[right] == nums[right-1] and left < right:
+                        right -=1
+                    right -=1
+            
         return res
                 
             
