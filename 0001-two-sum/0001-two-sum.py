d11d521ad8d206ad943559bb403914d3e1119a87
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-09 23:31:58 +0530

    Time: 2261 ms (37.43%), Space: 14.5 MB (6.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,10 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        hashmap={}
+        
         for i in range(len(nums)):
-            diff=target- nums[i]
-            if diff in hashmap:
-                return [i,hashmap[diff]]
-            hashmap[nums[i]]=i
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]== target:
+                    return [i,j]
+    
+            
+            
+            
+            
+#         hashmap={}
+#         for i in range(len(nums)):
+#             diff=target- nums[i]
+#             if diff in hashmap:
+#                 return [i,hashmap[diff]]
+#             hashmap[nums[i]]=i
             
