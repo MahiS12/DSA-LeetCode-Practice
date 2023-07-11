61622177f143f2513bd702f21e451409d92bf51e
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-12 01:52:25 +0530

    Time: 50 ms (48.37%), Space: 16.4 MB (43.68%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -7,25 +7,42 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         
+        ans = []
+        #dfs
         
-        if not root:
-            return []
-        
-        res = []
-        
-        q = deque([root])
-        
-        while q:
-            qlen = len(q)
+        def dfs(root,depth):
+            if not root:
+                return 
+            nonlocal ans
             
-            for _ in range(qlen):
+            if depth == len(ans): #if we are visiting a level first time,add the node
+                ans.append(root.val)
+            
+            dfs(root.right,depth+1)
+            dfs(root.left, depth+1)
+            
+        dfs(root,0)
+        return ans
+        
+#         #bfs
+#         if not root:
+#             return []
+        
+#         res = []
+        
+#         q = deque([root])
+        
+#         while q:
+#             qlen = len(q)
+            
+#             for _ in range(qlen):
                 
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
+#                 node = q.popleft()
+#                 if node.left:
+#                     q.append(node.left)
+#                 if node.right:
+#                     q.append(node.right)
                     
-            res.append(node.val)
-        return res
+#             res.append(node.val)
+#         return res
         
