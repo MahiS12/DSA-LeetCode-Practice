180981bfec6e5dcbd014fde41237fee1819d0281
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-15 18:43:10 +0530

    Time: 190 ms (54.95%), Space: 24.7 MB (98.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -6,19 +6,43 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
 
+        # n = len(nums)
+        # if k == n or k == 0:
+        #     return
+        # k= k % n
+        # if k > n:
+        #     return
+        # temp= [None]*n
+        # for i in range(n-k):
+        #     temp[i]= nums[i]
+        # for i in range(k):
+        #     nums[i]=nums[i+n-k]
+        # for i in range(k,n):
+        #     nums[i]=temp[i-k]
+            
+        
+        def rotate(nums,s,e):
+        
+            while s<e:
+                nums[s],nums[e] = nums[e],nums[s]
+                s+=1
+                e-=1
+        
+        
+        
         n = len(nums)
-        if k == n or k == 0:
-            return
-        k= k % n
-        if k > n:
-            return
-        temp= [None]*n
-        for i in range(n-k):
-            temp[i]= nums[i]
-        for i in range(k):
-            nums[i]=nums[i+n-k]
-        for i in range(k,n):
-            nums[i]=temp[i-k]
+        k = k%n
+        
+        rotate(nums,0,n-k-1)
+        rotate(nums,n-k,n-1)
+        rotate(nums,0,n-1)
+        
+        return nums
+        
+
+            
+            
+        
             
 
             
