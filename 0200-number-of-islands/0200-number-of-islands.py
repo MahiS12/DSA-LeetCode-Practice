94ae8e37d9e27ad7497cec93118df385b006bc9c
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-18 00:07:48 +0530

    Time: 315 ms (84.78%), Space: 18.6 MB (95.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,37 +1,58 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        if not grid:
-            return 0
-
-        rows, cols = len(grid), len(grid[0])
-        visited=set()
-        islands=0
-
-        def bfs(r,c):
-            q = deque()
-            visited.add((r,c))
-            q.append((r,c))
-           
-            while q:
-                row,col = q.popleft()
-                directions= [[1,0],[-1,0],[0,1],[0,-1]]
-               
-                for dr,dc in directions:
-                    r,c = row + dr, col + dc
-                    if (r in range(rows) and
-                    c in range(cols) and
-                    grid[r][c] == '1' and
-                    (r ,c) not in visited):
-                       
-                        q.append((r , c ))
-                        visited.add((r, c ))
-
+        #reduced space complexity
+        count = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in visited:
-                    bfs(r,c)
-                    islands +=1 
+                if grid[r][c] == "1":
+                    count += 1
+                    grid[r][c] = "0"
+                    queue = collections.deque()
+                    queue.append((r, c))
+                    while queue:
+                        cr, cc = queue.popleft()
+                        for dr, dc in directions:
+                            next_r, next_c = cr + dr, cc + dc
+                            if 0 <= next_r < rows and 0 <= next_c < cols and grid[next_r][next_c] == "1":
+                                grid[next_r][next_c] = "0"
+                                queue.append((next_r, next_c))
+        return count
+        
+#         if not grid:
+#             return 0
 
-        return islands
+#         rows, cols = len(grid), len(grid[0])
+#         visited=set()
+#         islands=0
+
+#         def bfs(r,c):
+#             q = deque()
+#             visited.add((r,c))
+#             q.append((r,c))
+           
+#             while q:
+#                 row,col = q.popleft()
+#                 directions= [[1,0],[-1,0],[0,1],[0,-1]]
+               
+#                 for dr,dc in directions:
+#                     r,c = row + dr, col + dc
+#                     if (r in range(rows) and
+#                     c in range(cols) and
+#                     grid[r][c] == '1' and
+#                     (r ,c) not in visited):
+                       
+#                         q.append((r , c ))
+#                         visited.add((r, c ))
+
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if grid[r][c] == "1" and (r,c) not in visited:
+#                     bfs(r,c)
+#                     islands +=1 
+
+#         return islands
         
