3397f6312205d2ebc3b5f037f33015a6dedca3e9
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-07-20 19:29:18 +0530

    Time: 139 ms (18.70%), Space: 17.8 MB (28.42%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,51 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        
+        # O(n) time and O(1) space
+        
+        ans1 = ans2 = -10**10
+        c1 = c2 = 0
+        n = len(nums)
+        
+        for i in range(len(nums)):
+            
+            if nums[i] == ans1:
+                c1+=1
+            elif nums[i] == ans2:
+                c2+=1
+            elif c1 == 0:
+                ans1 = nums[i]
+                c1=1
+            elif c2 == 0 :
+                ans2 = nums[i]
+                c2=1
+            else:
+                c1-=1
+                c2-=1
+            
+        # we might have only 1 majority element
+        
+        
+        count1 = 0
+        count2 = 0
+        for i in nums:
+            if ans1 == i:
+                count1+=1
+            
+            if ans2 == i:
+                count2+=1
+        
+        if count1 > n//3 and count2 > n//3:
+            return [ans1,ans2]
+        elif count2 > n//3:
+            return [ans2]
+        elif count1 > n//3:
+            return [ans1]
+        else:
+            return 
+        
+            
+        
+
+        
+        
