0906b0bceca778a34b69ca0373418c8bd16689ad
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-08-10 02:42:28 +0530

    Time: 191 ms (59.40%), Space: 17 MB (24.95%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        
+        d1 = {}
+        d2 = {}
+        res=0
+        #frequency of s
+        for i in s:
+            if i in d1:
+                d1[i]+=1
+            else:
+                d1[i]=1
+        #frequency of t        
+        for i in t:
+            if i in d2:
+                d2[i]+=1
+            else:
+                d2[i]=1
+                
+
+        
+        
+        for j in d1.keys():
+            if j in d2:
+                if d1[j] > d2[j]:
+                    res+= d1[j]-d2[j]
+            else:
+                res +=d1[j]
+        
+        return res
+
+        
