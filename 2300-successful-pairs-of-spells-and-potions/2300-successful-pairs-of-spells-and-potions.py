ef4ed9fb92270339b88ace28f487f2568cdbc577
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-12-29 14:05:48 +0530

    Time: 2418 ms (5.04%), Space: 40.3 MB (18.43%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,42 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        
+        
+        res = []
+        potions.sort()
+        
+        for i in spells:
+            #binary search
+            l,r = 0, len(potions)-1
+            index = len(potions)
+            
+            while l<=r:
+                mid = (l + r) //2 
+                 # weakest potion value to calculate the count
+                
+                if i * potions[mid] >= success:
+                    r = mid - 1
+                    index = mid
+                else:
+                    l = mid +1
+            
+            res.append(len(potions) - index)
+            
+        return res
+        
+#         res = []
+#         for i in range(len(spells)):
+#             count = 0
+#             for j in range(len(potions)):
+                
+#                 if spells[i] * potions[j] >= success:
+#                     count +=1
+            
+#             res.append(count)
+            
+        
+#         return res
+
+
+                
+        
