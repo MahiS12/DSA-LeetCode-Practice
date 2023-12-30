7ae194714d53a5247670fb16d2daced32e50607f
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2023-12-31 02:00:29 +0530

    Time: 394 ms (5.01%), Space: 24.8 MB (50.93%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,6 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        
+        n = len(nums)
+        
+        if k ==n or k ==0:
+            return
+        temp = [None]*n
+        for i in range(n):
+            temp[(i+k)%n] = nums[i]
+            
+        for i in range(n):
+            nums[i]=temp[i]
 
         # n = len(nums)
         # if k == n or k == 0:
@@ -21,23 +32,25 @@ class Solution(object):
         #     nums[i]=temp[i-k]
             
         
-        def rotate(nums,s,e):
+#         def rotate(nums,s,e):
         
-            while s<e:
-                nums[s],nums[e] = nums[e],nums[s]
-                s+=1
-                e-=1
+#             while s<e:
+#                 nums[s],nums[e] = nums[e],nums[s]
+#                 s+=1
+#                 e-=1
         
         
         
-        n = len(nums)
-        k = k%n
+#         n = len(nums)
+#         k = k%n
         
-        rotate(nums,0,n-k-1)
-        rotate(nums,n-k,n-1)
-        rotate(nums,0,n-1)
+#         rotate(nums,0,n-k-1)
+#         rotate(nums,n-k,n-1)
+#         rotate(nums,0,n-1)
         
-        return nums
+#         return nums
+
+        
         
 
             
