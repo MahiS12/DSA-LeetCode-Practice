2366651923e37563d4cf254f1bf1a0f72399f79f
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-01-02 14:28:23 +0530

    Time: 771 ms (92.70%), Space: 28.1 MB (15.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,19 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        
         i = 0
-        j =1
+        j =1 
         profit = 0
         
         while j < len(prices):
             curr = prices[j] - prices[i]
+            
             if prices[j] > prices[i]:
                 profit = max(profit,curr)
+            
             else:
-                i = j
+                i =j
             j+=1
-        return profit
-    
-
         
+        return profit
