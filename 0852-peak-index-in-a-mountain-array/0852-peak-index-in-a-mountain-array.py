79cc9c7b16f538370f250e7110a35c5f1bcb6237
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-01-05 13:23:43 +0530

    Time: 462 ms (61.64%), Space: 25.7 MB (81.68%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,20 +1,56 @@
 class Solution(object):
-    def peakIndexInMountainArray(self, nums):
+    def peakIndexInMountainArray(self, arr):
         """
         :type arr: List[int]
         :rtype: int
         """
         
         
-        low =0
-        high= len(nums)-1
+        l =1
+        r= len(arr)-2
+        
+        while l <=r:
+            mid = l +(r-l)//2
+            if arr[mid-1] <arr[mid] <arr[mid+1]:
+                l = mid+1
+            elif arr[mid-1] > arr[mid] >arr[mid+1]:
+                r = mid-1
+            else:
+                return mid
+        
+        
+        
+#         low = 0
+#         high = len(arr)-1
+        
+#         while low <=high:
+            
+#             mid = low + (high-low)//2
+#             if arr[mid]>=arr[mid-1] and arr[mid]>=arr[mid+1]:
+#                 return mid
+            
+#             if arr[mid] > arr[mid+1]: #decreasing part of array, look in left
+#                 high = mid-1  #checking the left half of the array for greater element
+                
+#             elif arr[mid] < arr[mid+1]: #look in right
+#                 low = mid +1 #checking the right half or array for greater
+                
+#         return low
 
-        while low <  high:
-            mid= low +(high-low)//2
 
-            if nums[mid] < nums[mid+1]:  #look in the right half
-                low = mid+1
-            elif nums[mid] > nums[mid+1]: #look in the left half
-                high = mid
-        return low
+#         low =0
+#         high= len(nums)-1
+
+#         while low <  high:
+#             mid= low +(high-low)//2
+
+#             if nums[mid] < nums[mid+1]:  #look in the right half
+#                 low = mid+1
+#             elif nums[mid] > nums[mid+1]: #look in the left half
+#                 high = mid
+#         return low
     
+            
+            
+            
+        
