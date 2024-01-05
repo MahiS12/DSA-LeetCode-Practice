42a1090a0b91c16c4a90e0c57a69a6a828e7d40a
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-01-05 17:41:25 +0530

    Time: 477 ms (31.03%), Space: 26 MB (6.03%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -6,37 +6,37 @@ class Solution(object):
         """
         
         #code 1
-        l =1
-        r= len(arr)-2
+#         l =1
+#         r= len(arr)-2
         
-        while l <=r:
-            mid = l +(r-l)//2
-            if arr[mid-1] <arr[mid] <arr[mid+1]:
-                l = mid+1
-            elif arr[mid-1] > arr[mid] >arr[mid+1]:
-                r = mid-1
-            else:
-                return mid
+#         while l <=r:
+#             mid = l +(r-l)//2
+#             if arr[mid-1] <arr[mid] <arr[mid+1]:
+#                 l = mid+1
+#             elif arr[mid-1] > arr[mid] >arr[mid+1]:
+#                 r = mid-1
+#             else:
+#                 return mid
 
         
 #         #code 2
         
-#         low = 0
-#         high = len(arr)-1
+        low = 0
+        high = len(arr)-1
         
-#         while low <=high:
+        while low <=high:
             
-#             mid = low + (high-low)//2
-#             if arr[mid]>=arr[mid-1] and arr[mid]>=arr[mid+1]:
-#                 return mid
+            mid = low + (high-low)//2
+            if arr[mid]>=arr[mid-1] and arr[mid]>=arr[mid+1]:
+                return mid
             
-#             if arr[mid] > arr[mid+1]: #decreasing part of array, look in left
-#                 high = mid-1  #checking the left half of the array for greater element
+            if arr[mid] > arr[mid+1]: #decreasing part of array, look in left
+                high = mid-1  #checking the left half of the array for greater element
                 
-#             elif arr[mid] < arr[mid+1]: #look in right
-#                 low = mid +1 #checking the right half or array for greater
+            elif arr[mid] < arr[mid+1]: #look in right
+                low = mid +1 #checking the right half or array for greater
                 
-#         return low
+        return low
 
 
             
