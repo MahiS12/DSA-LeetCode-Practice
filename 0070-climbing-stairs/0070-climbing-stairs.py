0bf7ee2c6d8a01928c054a29f7d9c0c344b2ead7
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-01-07 01:11:52 +0530

    Time: 20 ms (20.41%), Space: 13.2 MB (58.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -7,12 +7,21 @@ class Solution(object):
         if n==1 or n==2:
             return n
         
+        prev = curr =1
         
-        dp=[0]*(n+1)
-        dp[1]=1
-        dp[2]=2
+        for i in range(2,n+1):
+            temp = curr
+            curr=curr+prev
+            prev=temp
+        return curr
+            
         
-        for i in range(3,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
         
-        return dp[n]
+#         dp=[0]*(n+1)
+#         dp[1]=1
+#         dp[2]=2
+        
+#         for i in range(3,n+1):
+#             dp[i]=dp[i-1]+dp[i-2]
+        
+#         return dp[n]
