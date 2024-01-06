16a2b07cecd8087f6f0cfa1a8e4b241a14afafd2
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-01-06 23:02:12 +0530

    Time: 176 ms (85.68%), Space: 24.7 MB (33.97%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -2,26 +2,42 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
         n = len(nums)
-        prefix = [0]*n
-        postfix=[0]*n
+        res = [1]*n
+        prefix = 1
         
-        prefix[0]=1
-        postfix[n-1]=1
-        res=[]
-        
-        #construct prefix array
-        for i in range(1,n):
-            prefix[i]= prefix[i-1]* nums[i-1]
-        
-        #postfix
-        for i in range(n-2,-1,-1):
-            postfix[i] = postfix[i+1]*nums[i+1]
-        
-        #every index stores its prefix and postfix
-        for i in range(n):
-            res.append(postfix[i]*prefix[i])
+        for i in range(len(nums)):
+            res[i]=prefix
+            prefix=prefix*nums[i]
+            
+        postfix = 1
+        for i in range(n-1,-1,-1):
+            res[i] = res[i]*postfix
+            postfix= postfix*nums[i]
         
         return res
+        
+        
+#         n = len(nums)
+#         prefix = [0]*n
+#         postfix=[0]*n
+        
+#         prefix[0]=1
+#         postfix[n-1]=1
+#         res=[]
+        
+#         #construct prefix array
+#         for i in range(1,n):
+#             prefix[i]= prefix[i-1]* nums[i-1]
+        
+#         #postfix
+#         for i in range(n-2,-1,-1):
+#             postfix[i] = postfix[i+1]*nums[i+1]
+        
+#         #every index stores its prefix and postfix
+#         for i in range(n):
+#             res.append(postfix[i]*prefix[i])
+        
+#         return res
         
         
 
