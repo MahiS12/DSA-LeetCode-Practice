8749f9403af9902a294f13135b09dcfa87ee94ae
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-01-07 22:20:29 +0530

    Time: 97 ms (33.83%), Space: 14 MB (81.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,20 +4,48 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        visited = []
-        def zeroes(row,col):
-            
-            for i in range(0,len(matrix[0])):
-                matrix[row][i]=0
-                
-            for j in range(0,len(matrix)):
-                matrix[j][col]=0
+        rows,cols = len(matrix), len(matrix[0])
+        rowZero = False
         
-        for i in range(0,len(matrix)):
-            for j in range(0,len(matrix[0])):
-                if matrix[i][j]==0:
-                    visited.append((i,j))
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c]==0:
                     
-        for i,j in visited:
-            zeroes(i,j)
+                    matrix[0][c]=0
+                    if r > 0:
+                        matrix[r][0]=0
+                    else:
+                        rowZero= True
+        
+        
+        for r in range(1,rows):
+            for c in range(1,cols):
+                if matrix[r][0] ==0 or matrix[0][c]==0:
+                    matrix[r][c]=0
+        
+        if matrix[0][0]==0:
+            for r in range(rows):
+                matrix[r][0]=0
+        
+        if rowZero:
+            for c in range(cols):
+                matrix[0][c]=0
+        
+        
+#         visited = []
+#         def zeroes(row,col):
+            
+#             for i in range(0,len(matrix[0])):
+#                 matrix[row][i]=0
+                
+#             for j in range(0,len(matrix)):
+#                 matrix[j][col]=0
+        
+#         for i in range(0,len(matrix)):
+#             for j in range(0,len(matrix[0])):
+#                 if matrix[i][j]==0:
+#                     visited.append((i,j))
+                    
+#         for i,j in visited:
+#             zeroes(i,j)
         
