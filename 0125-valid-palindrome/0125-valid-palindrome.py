82c4e95a26a25a60197a738cf4a83f40fd37ba9c
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-01-15 19:25:38 +0530

    Time: 32 ms (61.44%), Space: 15.5 MB (28.40%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,11 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        if s == " ":
+            return True
+
+        alnum_letters = [i.lower() for i in s if i.isalnum()]
+
+        L = 0
+        R = len(alnum_letters) - 1
+
+        while L <= R:
+            if alnum_letters[L] == alnum_letters[R]:
+                L += 1
+                R -= 1
+            else:
+                return False
+
+        return True
         
-        
-        new = ''
-        for a in s:
-            if a.isalpha() or a.isdigit():
-                new += a.lower()
-        return (new == new[::-1])
+#         new = ''
+#         for a in s:
+#             if a.isalpha() or a.isdigit():
+#                 new += a.lower()
+#         return (new == new[::-1])
         
