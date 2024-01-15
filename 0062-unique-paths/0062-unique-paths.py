a646be3fad474e498c28454b4fc9f9329fdd0aa6
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-01-16 01:41:14 +0530

    Time: 17 ms (47.41%), Space: 13.1 MB (93.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -6,14 +6,22 @@ class Solution(object):
         :rtype: int
         """
         
-        row = [1] * n
-
-        for i in range(m - 1):
-            newRow = [1] * n
-            for j in range(n - 2, -1, -1):
-                newRow[j] = newRow[j + 1] + row[j]
-            row = newRow
+        row = [0] * (n-1) + [1]
+        
+        for i in range(m):
+            for j in range(n-2, -1, -1):
+                row[j] += row[j+1]
+        
         return row[0]
+    
+#         row = [1] * n
+
+#         for i in range(m - 1):
+#             newRow = [1] * n
+#             for j in range(n - 2, -1, -1):
+#                 newRow[j] = newRow[j + 1] + row[j]
+#             row = newRow
+#         return row[0]
 
         # O(n * m) O(n)
         
