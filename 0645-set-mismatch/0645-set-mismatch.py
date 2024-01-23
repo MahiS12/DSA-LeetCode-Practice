56b67018cc2af7b541696f99bdb2be9708567847
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-01-23 17:57:26 +0530

    Time: 146 ms (68.69%), Space: 13.6 MB (99.07%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -4,21 +4,40 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-
         n = len(nums)
-        v = [0] * (n + 1)
-        missing, duplicate = 0, 0
+        actual_sum = n * (n + 1) // 2
+        array_sum = 0
+        unique_sum = 0
+        s = set()
 
-        for num in nums:
-            v[num] += 1
+        for a in nums:
+            array_sum += a
 
-        for i in range(1, len(v)):
-            if v[i] == 2:
-                duplicate = i
-            if v[i] == 0:
-                missing = i
+        for a in nums:
+            s.add(a)
+
+        for a in s:
+            unique_sum += a
+
+        missing = actual_sum - unique_sum
+        duplicate = array_sum - unique_sum
 
         return [duplicate, missing]
+
+#         n = len(nums)
+#         v = [0] * (n + 1)
+#         missing, duplicate = 0, 0
+
+#         for num in nums:
+#             v[num] += 1
+
+#         for i in range(1, len(v)):
+#             if v[i] == 2:
+#                 duplicate = i
+#             if v[i] == 0:
+#                 missing = i
+
+#         return [duplicate, missing]
         
         
         
