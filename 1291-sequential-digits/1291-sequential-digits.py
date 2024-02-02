1678824a814eeb2b5a4b097bdd209887c66e6904
MahiS12 <106916988+MahiS12@users.noreply.github.com>
2024-02-02 20:43:08 +0530

    Time: 10 ms (93.10%), Space: 11.5 MB (100.00%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -5,22 +5,35 @@ class Solution(object):
         :type high: int
         :rtype: List[int]
         """
-        
         res = []
         
-        for i in range(1,10):
-            num = i
-            next_digit = i+1
-            
-            while num <= high and next_digit <=9:
-                num = num*10 + next_digit
+        string = "123456789"
+        n = len(str(low))
+        
+        for i in range(n,10):
+            for j in range(0,10-i):
+                numStr = string[j:j+i]
+                num = int(numStr)
                 
                 if low <= num <= high:
                     res.append(num)
-                next_digit+=1
-        
-        res.sort()
         return res
+        
+#         res = []
+        
+#         for i in range(1,10):
+#             num = i
+#             next_digit = i+1
+            
+#             while num <= high and next_digit <=9:
+#                 num = num*10 + next_digit
+                
+#                 if low <= num <= high:
+#                     res.append(num)
+#                 next_digit+=1
+        
+#         res.sort()
+#         return res
         
 #         res =[]
 #         for i in range(low,high+1):
